b16afe811216b67a888a22ef9bea952b7d1ada47
Harshvardhansinh-jadeja <harshvardhansinhjadeja49@gmail.com>
2024-04-26 12:15:40 +0530

    Added UTF-8 format for tfvars file

--- scripts/ssm.ps1
+++ scripts/ssm.ps1
@@ -1,10 +1,21 @@
-$PARAMS = @("region", "invoke_url")
+$PARAMS = @("region")
 $REGION = "us-west-2"
 $PREFIX = "/harshvardhan/personal/account/"
-$TFVARS_FILE = "../Infrastructure/terraform.tfvars"
+$TFVARS_FILE = "terraform.tfvars"
+$NEW_LOCATION = "../Infrastructure"
 
 foreach ($param in $PARAMS) {
     $value = aws --region $REGION ssm get-parameter --name "$PREFIX$param" --with-decryption --output text --query Parameter.Value 
-    "$param = `"$value`"" | Out-File -FilePath $TFVARS_FILE -Append
+    "$param = `"$value`"" | Out-File -FilePath $TFVARS_FILE -Append 
 }
 
+# Read the content from terraform.tfvars file
+$rawContent = Get-Content -Raw $TFVARS_FILE
+
+# Define UTF-8 encoding without BOM
+$utf8NoBomEncoding = New-Object System.Text.UTF8Encoding $False
+
+# Write the content back to the file with UTF-8 encoding without BOM
+[System.IO.File]::WriteAllText($TFVARS_FILE, $rawContent, $utf8NoBomEncoding)
+
+Move-Item -Path $TFVARS_FILE -Destination $NEW_LOCATION
